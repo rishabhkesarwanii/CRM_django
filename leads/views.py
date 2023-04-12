bd40e54373dbbe2ed84f707d98cc202c11282a0f
Rishabh Kesarwani <rkesari2002@gmail.com>
2023-04-12 21:48:51 +0530

    URL Names

--- leads/views.py
+++ leads/views.py
@@ -46,6 +46,11 @@ def lead_update(request, pk):
         }
     return render(request, "leads/lead_update.html", context)
 
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
+
 # def lead_create(request):
     # form = LeadForm()
     # if request.method == "POST":
