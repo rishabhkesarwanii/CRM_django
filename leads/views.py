17b809756d5b832ca77f1831632d283912e91584
Rishabh Kesarwani <rkesari2002@gmail.com>
2023-04-12 17:11:03 +0530

    Lead List and Build CRM

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
-def home_page(request):
-    return render(request, "leads/home_page.html", {})
+def lead_list(request):
+    lead = Lead.objects.all()
+
+    context = {
+        "leads": lead
+        }
+
+    return render(request, "leads/lead_list.html", context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+
+    context = {
+        "lead": lead
+        }
+
+    return render(request, "leads/lead_detail.html", context)
