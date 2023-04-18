146691c2e54ca295b097e5bc84b26c40470bb840
Rishabh Kesarwani <rkesari2002@gmail.com>
2023-04-18 10:14:47 +0530

    Unassigned lead

--- leads/views.py
+++ leads/views.py
@@ -40,12 +40,32 @@ class LeadListView(LoginRequiredMixin, ListView):
         user = self.request.user
         # initial queryset of leads for the entire organisation
         if user.is_organiser:
-            queryset = Lead.objects.filter(organisation=user.userprofile)
+            queryset = Lead.objects.filter(
+                organisation=user.userprofile, 
+                agent__isnull=False
+            )
         else:
-            queryset = Lead.objects.filter(organisation=user.agent.organisation)
+            queryset = Lead.objects.filter(
+                organisation=user.agent.organisation, 
+                agent__isnull=False
+            )
             # filter for the agent that is logged in
             queryset = queryset.filter(agent__user=user)
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super(LeadListView, self).get_context_data(**kwargs)
+        user = self.request.user
+        if user.is_organiser:
+            queryset = Lead.objects.filter(
+                organisation=user.userprofile, 
+                agent__isnull=True
+            )
+            context.update({
+                "unassigned_leads": queryset
+            })
+        return context
+
 
 # def lead_list(request):
 #     lead = Lead.objects.all()
@@ -70,6 +90,9 @@ class LeadDetailView(LoginRequiredMixin, DetailView):
             # filter for the agent that is logged in
             queryset = queryset.filter(agent__user=user)
         return queryset
+    
+
+
 
 # def lead_detail(request, pk):
 #     lead = Lead.objects.get(id=pk)
